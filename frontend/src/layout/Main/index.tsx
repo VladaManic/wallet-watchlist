69599fed12bca279f072ca3b17cb620884a22015
VladaManic <65961459+VladaManic@users.noreply.github.com>
2026-08-04 16:01:09 +0200

    routing

--- frontend/src/layout/Main/index.tsx
+++ frontend/src/layout/Main/index.tsx
@@ -1,6 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../../pages/Home'
+import Single from '../../pages/Single'
+import Add from '../../pages/Add'
+import Page404 from '../../pages/Page404'
+
 const Main = () => {
 	return (
-		<div>Main</div>
+		<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/wallet/:walletId" element={<Single />} />
+				<Route path="/add" element={<Add />} />
+				<Route path="*" element={<Page404 />} />
+		</Routes>
 	)
 }
 
