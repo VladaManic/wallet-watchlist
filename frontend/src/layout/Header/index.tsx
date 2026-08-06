3c853d2432f95cb16dea59de56816790b9177b01
VladaManic <65961459+VladaManic@users.noreply.github.com>
2026-08-06 12:08:23 +0200

    header

--- frontend/src/layout/Header/index.tsx
+++ frontend/src/layout/Header/index.tsx
@@ -1,6 +1,19 @@
+import { NavLink } from 'react-router-dom'
+
+import walletIcon from '../../assets/img/blockchain-wallet.png'
+
 const Header = () => {
 	return (
-		<div>Header</div>
+		<div className="flex justify-between items-center w-full h-[52px] pt-[40px] pb-[40px] bg-header-bg">
+			<NavLink to={`/`} end className="ml-[50px]">
+				<img src={walletIcon} alt="Wallet icon" className="h-[60px]" />
+			</NavLink>
+			<div>
+				<NavLink to={`/`} end className="mr-[20px] ml-[20px]">Home</NavLink>
+				<NavLink to={`/add`} className="mr-[20px] ml-[20px]">Add Wallet</NavLink>
+			</div>
+			<div></div>
+		</div>
 	)
 }
 
