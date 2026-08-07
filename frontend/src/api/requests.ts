36e3aa54b3ecb809d314736548b31fe559bcd08f
VladaManic <65961459+VladaManic@users.noreply.github.com>
2026-08-07 15:04:46 +0200

    typescript correction

--- frontend/src/api/requests.ts
+++ frontend/src/api/requests.ts
@@ -1,9 +1,9 @@
 import { api } from './axios';
+import type { WalletObj } from '../types/interfaces';
 
-export const getWalletsList = async () => {
+export const getWalletsList = async (): Promise<WalletObj[]> => {
     try {
-        const response = await api.get('/api/wallets');
-        //console.log(response.data);
+        const response = await api.get<WalletObj[]>('/api/wallets');
         return response.data;
     } catch (error) {
         console.error('Failed to fetch wallets:', error);
